60d6682789a337d2f652fbdc13aeecc8567caf9a
David Sunderland <dnsunderland@gmail.com>
2013-03-19 22:44:31 -0700

    Add explanatory comments.

--- src/gc/grooviks1.py
+++ src/gc/grooviks1.py
@@ -1,5 +1,13 @@
 '''
-Created on Mar 16, 2013
+Created on Mar 16, 2013.
+
+This server simulates a simple single player Rubik's Cube game session.  It
+simply allows clients to create games and to update the server with the state
+of their local game.
+
+Instructions to run:
+
+  python gc/grooviks1.py <port>
 
 @author: David
 '''
@@ -29,12 +37,16 @@ def mr(target):
         return target(*args)
     return wrapped
 
+# URL routing for the server
 urls = (
     '/gc1/games(/)?', 'Games',
     '/gc1/games/(\d+)', 'Game',
 )
 app = web.application(urls, globals())
 
+# Global state.
+# TODO(david): replace with something more persistent, or at least shared
+#  between server instances.
 _next_game_id = 1
 _games = {}
 
@@ -44,6 +56,14 @@ def games():
 def game(game_id):
     return _games[game_id]
 
+# Wacky custom representation of cube.  This view show the cube as if it were
+# unfolded into the shape of a cross, with the literal colors of the facets
+# recorded at each location.  The 3x3 square at the center of the cross is the
+# front of the cube (i.e. the "F" face, in Singmaster notation); the right and
+# left arms are R and L, respectively, the top arm is U, and the bottom arm is
+# D (down) then B (back) at the bottom.
+#
+# TODO(david): see if we can find a more standard representation 
 _solved = { 'format': 'cross', 'repr': [ 
 	'   rrr   ',
 	'   rrr   ',
@@ -119,15 +139,19 @@ class Game:
 # GET /admin
 #
 class Admin:
+    # TODO
     pass
 
+#
+# API for adding physical cubes to the game server so that they may be attached
+# to games.
 #
 # GET /cubes
 # POST /cubes { physical cube self-registration }
 #
 class Cubes:
+    # TODO
     pass
 
 if __name__ == '__main__':
     app.run()
-
